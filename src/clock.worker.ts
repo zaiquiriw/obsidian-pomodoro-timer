cf4f6029fc73b4e4b4478ddf63bc8ea797c509f7
Eatgrass <eatgrass@live.cn>
2024-05-28 10:40:35 +0800

    feat: low fps mode (#57)

--- src/clock.worker.ts
+++ src/clock.worker.ts
@@ -1,19 +1,47 @@
 let running: boolean = false
+let start: number | undefined
+let startTime: number | undefined
+let interval: number
+let prev: number | undefined
+let lowFps: boolean = false
 
-const tick = (): void => {
+const tick = (t: number): void => {
+    if (start === undefined) {
+        start = t
+        prev = t
+    }
+
+    interval += t - prev!
+
     if (running) {
-        self.postMessage(new Date().getTime())
+        const elapsed = t - start
+		if(lowFps) {
+			if (interval >= 1000) {
+				self.postMessage(startTime! + elapsed)
+				interval = 0
+			}
+		} else {
+			self.postMessage(startTime! + elapsed)
+		}
         requestAnimationFrame(tick)
     }
+
+    prev = t
 }
 
 self.onmessage = async ({ data }) => {
-    if (data) {
+    if (data.start) {
+		lowFps = data.lowFps
         if (!running) {
             running = true
-            tick()
+            interval = 0
+            startTime = new Date().getTime()
+            requestAnimationFrame(tick)
         }
     } else {
         running = false
+        start = undefined
+        startTime = undefined
+        prev = undefined
     }
 }
